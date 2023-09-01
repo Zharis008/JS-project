84ee05daa2cc037726756bf4733de98c54cefcdf
Zharis008 <Zharis008@gmail.com>
2023-09-01 08:09:12 -0700

    js project

---
+++ script.js
@@ -0,0 +1,66 @@
+                              //   CHAPTER NO 1        
+                                     //  Q NO 1
+
+/*var helloMessage = "hello";
+alert(helloMessage);*/
+
+                                       // Q NO 2
+// var errorMessage = "Error! please enter a valid password";
+//  alert(errorMessage);
+
+                                         //Q No 3
+
+// var jsLandMessage = "Welcome to js land... \nHappy Coding";
+// alert(jsLandMessage);
+                                          //Q No 4
+
+// var greetingMessage = "";
+//   alert(greetingMessage);
+                                      //Q No 5
+
+// var webBrowser = "Hello..I can run JS through my web browser's console ";
+//  alert(webBrowser);
+
+                               
+                                // CHAPTER NO 2 
+                               
+                                //  Q No 1
+
+// var userName ="Zaid Haris";
+// alert(userName);
+
+                                   //Q No 2
+//  var age ="15 Years old";
+// alert(age);
+                                   
+
+                                         //Q No 3
+//   var mobileDev ="Certified Mobile Application Development";
+//   alert(mobileDev);
+                              
+
+                                         //Q No 4
+
+//   var pizza ="PIZZA\nPIZZ\nPIZ\nPI\nP";
+//   alert(pizza);
+
+                                          //Q No 5
+
+//   var emailAlert ="My Email Address Is zharis008@gmail.com";
+//  alert(emailAlert);
+
+                                       //Q No 6
+
+// var learnJavascript ="I am trying to learn form the BOOk A Smarter\nWay to learn javascript";
+// alert(learnJavascript);
+
+                                       //Q No 7
+
+// var greetingMessage ="Yah! i can write HTML content through JavaScript";
+// alert(greetingMessage);
+
+                                      //Q No 8
+
+ var specialAlert ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+  alert(specialAlert);
+                                      
